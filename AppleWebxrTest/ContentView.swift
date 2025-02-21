241f11a277580b0b10c49694bfe694f008a85fd1
Graham Reeves <graham@grahamreeves.com>
2025-02-21 15:07:53 +0000

    Initial version loading webview with some xr polyfil

--- AppleWebxrTest/ContentView.swift
+++ AppleWebxrTest/ContentView.swift
@@ -4,21 +4,210 @@
 //
 //  Created by Graham Reeves on 21/02/2025.
 //
-
 import SwiftUI
+import WebKit
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+//	macos -> ios aliases to make things a little cleaner to write
+#if canImport(UIKit)
+import UIKit
+#else//macos
+import AppKit
+public typealias UIView = NSView
+public typealias UIColor = NSColor
+public typealias UIRect = NSRect
+public typealias UIViewRepresentable = NSViewRepresentable
+#endif
+
+
+
+let RequestAnimationFramePolyfill = """
+var lastTime = 0;
+window.requestAnimationFrame = function(callback, element) {
+	var now = window.performance.now();
+	var nextTime = Math.max(lastTime + 16, now); //First time will execute it immediately but barely noticeable and performance is gained.
+	return setTimeout(function() { callback(lastTime = nextTime); }, nextTime - now);
+};
+window.cancelAnimationFrame = clearTimeout;
+"""
+
+let CustomHandlerApiSource = """
+function CallCustomHandler()
+{
+	window.webkit.messageHandlers.CustomContent.postMessage("Hello");
+}
+CallCustomHandler()
+"""
+
+
+
+//	load custom script
+//	https://stackoverflow.com/a/58615934/355753
+
+
+class WebxrController : NSObject, WKScriptMessageHandler
+{
+	func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) 
+	{
+		print("CustomHandlerApi callback")
+	}
+}
+
+
+class WebViewController
+{
+	var enableDebug = true
+	var webXr = WebxrController()
+	var webView : WKWebView!
+	
+	init()
+	{
+		self.webView = CreateWebView()
+	}
+	
+	public func LoadUrl(url:String)
+	{
+		let request = URLRequest(url: URL(string: url)! )
+		webView.load(request)
+	}
+	
+
+	func CreateWebView() -> WKWebView
+	{
+		let configuration = GetConfiguration()
+		let frame = CGRect.zero
+		let webView = WKWebView(frame:frame,configuration:configuration)
+		
+		if enableDebug
+		{
+			if webView.responds(to: Selector(("setInspectable:"))) 
+			{
+				webView.perform(Selector(("setInspectable:")), with: true)
+			}
+		}
+		
+		return webView
+	}
+
+	func GetConfiguration() -> WKWebViewConfiguration
+	{
+		let configuration = WKWebViewConfiguration()
+		
+		let WebxrPolyfillUrl = Bundle.main.url(forResource: "WebxrPolyfill", withExtension: "js")!
+		let WebxrPolyfillCode = try! String(contentsOf: WebxrPolyfillUrl, encoding: .utf8)
+		let webxrPolyfill = WKUserScript(source: WebxrPolyfillCode, injectionTime: .atDocumentStart, forMainFrameOnly: false)
+		configuration.userContentController.addUserScript(webxrPolyfill)
+		
+		let CustomHandlerApi = WKUserScript(source: CustomHandlerApiSource, injectionTime: .atDocumentStart, forMainFrameOnly: false)
+		configuration.userContentController.addUserScript(CustomHandlerApi)
+		
+		//	only needed for preview, but still doesnt work great
+		//let RequestAnimationFramePolyfill = WKUserScript(source: RequestAnimationFramePolyfill, injectionTime: .atDocumentStart, forMainFrameOnly: false)
+		//configuration.userContentController.addUserScript(RequestAnimationFramePolyfill)
+		
+		//	js: window.webkit.messageHandlers.CustomContent.postMessage(messageText);
+		configuration.userContentController.add(webXr, name: "CustomContent")
+		
+		return configuration
+	}
+}
+
+//	macos needs "allow outgoing connections (client)" to get wkwebview to display content
+struct WebView : UIViewRepresentable
+{
+	@Binding var webviewController : WebViewController
+	
+	public typealias UIViewType = WKWebView
+	public typealias NSViewType = WKWebView
+		
+	public func makeUIView(context: Context) -> UIViewType
+	{
+		return webviewController.webView
+	}
+	
+	public func makeNSView(context: Context) -> NSViewType
+	{
+		return makeUIView(context: context)
+	}
+	
+	func updateUIView(_ uiView: WKWebView, context: Context)
+	{
+		//uiView.
+	}
+	
+	func updateNSView(_ nsView: WKWebView, context: Context) 
+	{
+		updateUIView( nsView, context:context)
+	}
+}
+/*
+
+class ViewController: UIViewController, WKUIDelegate {
+	
+	var webView: WKWebView!
+	
+	override func loadView() {
+		let webConfiguration = WKWebViewConfiguration()
+		webView = WKWebView(frame: .zero, configuration: webConfiguration)
+		webView.uiDelegate = self
+		view = webView
+	}
+	
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		let myURL = URL(string:"https://www.apple.com")
+		let myRequest = URLRequest(url: myURL!)
+		webView.load(myRequest)
+	}
+}
+*/
+struct ContentView: View 
+{
+	//let url = "https://webglsamples.org/blob/blob.html"
+	//let url = "https://immersive-web.github.io/webxr-samples/immersive-ar-session.html"
+	//@State var url : String = "https://immersive-web.github.io/webxr-samples/immersive-ar-session.html?usePolyfill=0"
+	@State var url : String = "https://immersive-web.github.io/webxr-samples/"
+	
+	@State var webViewController = WebViewController()
+
+	func OnReload()
+	{
+		webViewController.LoadUrl(url: url)
+	}
+	
+	func NavigationView() -> some View
+	{
+		HStack
+		{
+			Text(self.url)
+			Button(action:OnReload)
+			{
+				Label("Reload", systemImage:"arrow.clockwise.circle.fill")
+			}
+		}
+		.padding(10)
+		.background(.blue)
+		.foregroundStyle(.white)
+	}
+	
+	var body: some View 
+	{
+		WebView(webviewController: self.$webViewController)
+			//.edgesIgnoringSafeArea(.all)
+			.background(.red)
+			.overlay(alignment:.topTrailing)
+		{
+			NavigationView()
+		}
+		.onAppear()
+		{
+			OnReload()
+		}
+	}
 }
 
 #Preview {
-    ContentView()
+	ContentView()
 }
+
---
+++ AppleWebxrTest/WebxrPolyfill.js
@@ -0,0 +1,194 @@
+//	todo: use https://github.com/immersive-web/webxr-polyfill/tree/ polyfill
+//const polyfill = import('https://cdn.jsdelivr.net/npm/webxr-polyfill@latest/build/webxr-polyfill.js')
+
+//	https://developer.mozilla.org/en-US/docs/Web/API/XRViewport
+class XRViewport
+{
+	get x()	{	return 0;	}
+	get y()	{	return 0;	}
+	get width()	{	return 400;	}
+	get height()	{	return 400;	}
+}
+
+class XRWebGLLayer
+{
+	constructor(session,glcontext)
+	{
+		console.log("XRWebGLLayer");
+	}
+	
+	getViewport(view/*XRView*/)
+	{
+		return new XRViewport();
+	}
+	
+	//	null == inline framebuffer
+	get framebuffer()	{	return null;	}
+}
+
+class XRRigidTransform
+{
+	constructor()
+	{
+		/*
+		 let xform = new XRRigidTransform(
+		 {},
+		 {x: invOrient[0], y: invOrient[1], z: invOrient[2], w: invOrient[3]});
+		 this.refSpace = this.baseRefSpace.getOffsetReferenceSpace(xform);
+		 xform = new XRRigidTransform({y: -this.viewerHeight});
+*/
+	}
+	
+	static CreateIdentity()	{	return new XRRigidTransform();	}
+	get matrix()	{	return [1,0,0,0,	0,1,0,0,	0,0,1,0,	0,0,0,1];	}
+	get position()	{	return {x:0,y:0,z:0};	}
+	get inverse()	{	return new XRRigidTransform();	}
+}
+
+
+//	https://developer.mozilla.org/en-US/docs/Web/API/XRView
+class XRView
+{
+	get eye()	{	return 'left';	}
+	/*
+	view ? view.projectionMatrix : null,
+	view ? view.transform : null,
+	viewport ? viewport : ((layer && view) ? layer.getViewport(view) : null),
+	view ? view.eye : 'left'
+	 */
+	get projectionMatrix()	{	return [1,0,0,0,	0,1,0,0,	0,0,1,0,	0,0,0,1];	}
+	get transform()	{	return XRRigidTransform.CreateIdentity();	}
+}
+
+class XRPose
+{
+	get views()	{	return [new XRView()];	}
+}
+
+class XRFrame
+{
+	constructor(session)
+	{
+		this.session = session;
+	}
+	
+	getViewerPose(referenceSpace)
+	{
+		return new XRPose();
+	}
+}
+
+
+const ReferenceSpaceType =
+{
+	local: "Local",
+	viewer: "Viewer"
+};
+
+class ReferenceSpaceInstance
+{
+	//xrImmersiveRefSpace.addEventListener('reset', (evt) => {
+	//xrImmersiveRefSpace = xrImmersiveRefSpace.getOffsetReferenceSpace(evt.transform);
+	//	https://developer.mozilla.org/en-US/docs/Web/API/XRReferenceSpace/getOffsetReferenceSpace
+	getOffsetReferenceSpace(Transform)
+	{
+		return new ReferenceSpaceInstance();
+	}
+}
+
+class ArSession
+{
+	#EventCallbacks = {};
+	
+	//	immersive = see through
+	#IsImmersive = false;
+	
+	#RenderState = null;
+	
+	get isImmersive()		{	return this.#IsImmersive == true;	}
+	set isImmersive(value)	{	}//this.#IsImmersive = value;	}
+
+	get renderState()		{	return this.#RenderState;	}
+	
+	//	user ended session
+	end()
+	{
+		//console.log("User ended session");
+	}
+	
+	addEventListener(eventName,Callback)
+	{
+		this.#EventCallbacks[eventName] = Callback;
+	}
+	
+	updateRenderState(renderState)
+	{
+		this.#RenderState= renderState;
+		const baseLayer = renderState.baseLayer;
+		//{ baseLayer: new XRWebGLLayer(session, gl) });
+	}
+	
+	async requestReferenceSpace(referenceSpaceType)
+	{
+		return new ReferenceSpaceInstance();
+	}
+	
+	requestAnimationFrame(onXRFrame)
+	{
+		function Tick()
+		{
+			const Time = 1000;
+			const Frame = new XRFrame(this);
+			onXRFrame( Time, Frame );
+		}
+		
+		setTimeout( Tick.bind(this), 100 );
+	}
+	/*
+	OnReset()
+	{
+		let ResetEvent = {};	//.transform
+		this.#EventCallbacks.reset?(ResetEvent);
+		
+	}
+	*/
+}
+
+
+
+
+const SessionClasses = 
+{
+	'inline': ArSession,
+	'immersive-ar': ArSession
+};
+
+
+class XrPolyfill
+{
+	#ActiveSession = null;
+	
+	FreeSession()
+	{
+		this.#ActiveSession?.end();
+		this.#ActiveSession = null;
+	}
+	
+	async isSessionSupported(sessionType)
+	{
+		const SessionClass = SessionClasses[sessionType];
+		return SessionClass != undefined;
+	}
+	
+	async requestSession(sessionType)
+	{
+		const SessionClass = SessionClasses[sessionType];
+		if ( !SessionClass )
+			//throw new NotSupportedError();
+			throw `No such session type for ${sessionType}`;
+		this.#ActiveSession = new SessionClass();
+		return this.#ActiveSession;
+	}
+}
+
+navigator.xr = new XrPolyfill();
