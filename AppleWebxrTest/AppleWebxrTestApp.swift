3b956db872ac4840422ae2e64162196a087fd55f
Graham Reeves <graham@grahamreeves.com>
2025-02-21 10:49:13 +0000

    Initial Commit

---
+++ AppleWebxrTest/AppleWebxrTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleWebxrTestApp.swift
+//  AppleWebxrTest
+//
+//  Created by Graham Reeves on 21/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppleWebxrTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
